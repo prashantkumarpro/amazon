ce6b10ad1b18a0ae4442e17d6b7954c79bf7d44a
Prashant Kumar <programmerkrprashant@gmail.com>
2024-01-08 22:19:41 +0530

    Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,17 +16,26 @@ const Sidebar = ({ setSideBar, Sidebar }) => {
   return (
     <>
       <motion.div
+        className='sidebar-con'
         ref={ref}
         initial={{ x: -500, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: .2 }}
-        className='sidebar-con'>
+        transition={{ duration: .2 }}>
         <div className="content">
           <div className="account">
-            <i class="ri-account-circle-fill account-icon"></i>
-            <p>Hello, Sign In</p>
+            <i className="ri-account-circle-fill account-icon"></i>
+            <h3>Hello, Sign In</h3>
+          </div>
+          <div className='content-details'>
+            <h3 style={{marginBottom:'10px', padding:"0 6px"}}>Digital Content And Devices</h3>
+            <ul className='links'>
+              <li>Echo & Alexa <span><i class="ri-arrow-right-s-line"></i></span></li>
+              <li>Echo & Alexa <span><i class="ri-arrow-right-s-line"></i></span></li>
+              <li>Echo & Alexa <span><i class="ri-arrow-right-s-line"></i></span></li>
+            </ul>
           </div>
         </div>
+
         <i className="ri-close-fill close-sidebar" onClick={() => setSideBar(false)}></i>
       </motion.div>
     </>
