07de4985cdf7fb0616d9e35d1908a6e3e72a9043
Prashant Kumar <programmerkrprashant@gmail.com>
2024-01-10 19:33:28 +0530

    sidebar

--- src/components/SidebarContent/SidebarConten.jsx
+++ src/components/SidebarContent/SidebarConten.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import './SidebarContent.css'
+import { Link } from 'react-router-dom';
+
+const handleOnClick = (title, item) => {
+    console.log(`Clicked on ${title}: ${item}`);
+    // Add your custom logic here
+  
+  };
 const SidebarConten = ({ title, one, two, three, four }) => {
+    let strings = one;
+    let myArray = one.split(" ");
+    let word = myArray[2]
+    // console.log(myArray)
+    console.log(myArray[0])
     return (
 
         <div className='content-con'>
             <h3>{title}</h3>
             <ul>
-                <li>{one}<span><i className="ri-arrow-right-s-line"></i></span></li>
-                <li>{two} <span><i className="ri-arrow-right-s-line"></i></span></li>
-                <li>{three}<span><i className="ri-arrow-right-s-line"></i></span></li>
-                <li>{four}<span><i className="ri-arrow-right-s-line"></i></span></li>
+                <li onClick={() => handleOnClick(title, one)}>{one}<span><i className="ri-arrow-right-s-line"></i></span></li>
+                <li  onClick={() => handleOnClick(title, two)}>{two} <span><i className="ri-arrow-right-s-line"></i></span></li>
+                <li  onClick={() => handleOnClick(title, three)}>{three}<span><i className="ri-arrow-right-s-line"></i></span></li>
+                <li  onClick={() => handleOnClick(title, four)}>{four}<span><i className="ri-arrow-right-s-line"></i></span></li>
             </ul>
         </div>
 
