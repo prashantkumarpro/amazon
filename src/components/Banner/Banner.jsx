6b2284fff530f8820a571de52075415d49dd3c97
Prashant Kumar <programmerkrprashant@gmail.com>
2024-01-12 15:57:02 +0530

    Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,7 +9,8 @@ const Banner = () => {
         'https://images-eu.ssl-images-amazon.com/images/G/31/prime/JanART/GW/T2/PC_Hero_1500x600_deals_ft._CB585907150_.jpg',
         'https://images-eu.ssl-images-amazon.com/images/G/31/imgg/hero/1499/30th/nov/hero1300x1200._CB572143637_.jpg',
         'https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg',
-        'https://images-eu.ssl-images-amazon.com/images/G/31/img2020/img21/apparelGW/jan24atf/unrec/hsbc/WA_WW_3000._CB585877492_.jpg'
+        'https://images-eu.ssl-images-amazon.com/images/G/31/img2020/img21/apparelGW/jan24atf/unrec/hsbc/WA_WW_3000._CB585877492_.jpg',
+        'https://images-eu.ssl-images-amazon.com/images/G/31/img24/Fashion/Event/Gateway/JanART/Teaser/rev/6._CB586013332_.jpg'
     ]
     return (
         <Carousel
@@ -18,14 +19,16 @@ const Banner = () => {
             animation='slide'
             indicators={false}
             cycleNavigation={true}
+            navButtonsAlwaysVisible={true}
             navButtonsProps={{
                 style: {
-                    backgroundColor: '#fff',
-                    color: 'black',
-                    width: '50px',
-                    height: '150px',
-                    borderRadius: '5px'
-
+                    backgroundColor: 'transparent',
+                    border: '1.5px solid transparent',
+                    color: '#494949',
+                    height: '230px',
+                    borderRadius: 0,
+                    marginTop: -94.50,
+                    width:'80px'
                 }
             }}
 
