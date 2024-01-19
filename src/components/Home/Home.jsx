6edbb5d71a1c3f1b24270aad0469a405e73d3fb3
Prashant Kumar <programmerkrprashant@gmail.com>
2024-01-19 20:53:06 +0530

    Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -104,6 +104,86 @@ const Home = () => {
 
           </div>
         </div>
+        <div className="card-con">
+          <div className="card">
+            <h3 style={{ fontSize: '18px', marginTop: '5px' }}>Men's Clothing</h3>
+            <div className="box-con">
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+
+              </div>
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="" />
+              </div>
+            </div>
+            <div className="box-con">
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" alt="" />
+              </div>
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg" alt="" />
+              </div>
+            </div>
+            <Link to="/Product">See more</Link>
+          </div>
+          <div className="card">
+            <h3 style={{ fontSize: '18px', marginTop: '5px' }}>Keep shopping for</h3>
+            <div className="box-con">
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg" alt="" />
+
+              </div>
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg" alt="" />
+              </div>
+            </div>
+            <div className="box-con">
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+              </div>
+              <div className="box">
+                <img src="https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg" alt="" />
+              </div>
+            </div>
+            <Link to="/Product">See more</Link>
+          </div>
+          <div className="card">
+            <h3 style={{ fontSize: '18px', marginTop: '5px' }}>Deals on smartphones</h3>
+            <div className="box-con">
+              <div className="box">
+                <img src="https://m.media-amazon.com/images/I/61bUETZj9AL._AC_UY218_.jpg" alt="" />
+              </div>
+              <div className="box">
+                <img src="https://m.media-amazon.com/images/I/71nj3tkXZXL._AC_UL320_.jpg" alt="" />
+              </div>
+            </div>
+            <div className="box-con">
+              <div className="box">
+                <img src="https://m.media-amazon.com/images/I/81bh6SxXQ+L._AC_UL320_.jpg" alt="" />
+              </div>
+              <div className="box">
+                <img src="https://m.media-amazon.com/images/I/51HnWtX7klL._AC_UY218_.jpg" alt="" />
+              </div>
+            </div>
+            <Link to="/Mobile">See more</Link>
+          </div>
+          <div className="card bg_transparent">
+            
+            <div className="box1">
+              <h3 style={{ fontSize: '18px', marginTop: '5px' }}>ASUS VIVOBOOK</h3>
+              <div className="item_con">
+                <Link> Stylish Everyday Laptops</Link>
+                <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/JanART2024_ASUN/115x115._CB584223445_.jpg" alt="image" />
+              </div>
+            </div>
+
+            <div className="box2">
+              <img src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/16bac034-e089-426c-bf00-228b58692926.jpg" alt="" />
+            
+            </div>
+
+          </div>
+        </div>
 
         <ProductCarousel title='Blockbuster deals' />
         <ProductCarousel title='Trending deals' />
